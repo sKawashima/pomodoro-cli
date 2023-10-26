e16b3c3001649a0d0b1e30b98edcad2da632da53
sKawashima <bysk.333@gmail.com>
2023-10-26 17:08:16 +0900

    Update by AI

--- main.js
+++ main.js
@@ -1,42 +1,51 @@
-const notifier = require('node-notifier');
 const readline = require('readline');
-
-const startTimer = (duration, display) => {
-    let timer = duration, minutes, seconds;
-    setInterval(() => {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? '0' + minutes : minutes;
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-
-        readline.clearLine(process.stdout, 0);
-        readline.cursorTo(process.stdout, 0, null);
-        process.stdout.write(minutes + ':' + seconds);
-
-        if (--timer < 0) {
-            timer = duration;
-            notifier.notify({
-                title: 'Pomodoro Timer',
-                message: 'Time is up!',
-                sound: true
-            });
-        }
-    }, 1000);
-}
+const notifier = require('node-notifier');
 
 const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+  input: process.stdin,
+  output: process.stdout
 });
 
-const startPomodoro = () => {
-    rl.question('Press Enter to start the Pomodoro timer...', (answer) => {
-        startTimer(25 * 60, process.stdout);
-        setTimeout(() => {
-            startTimer(5 * 60, process.stdout);
-        }, 25 * 60 * 1000);
-    });
+const workTime = 25 * 60;
+const breakTime = 5 * 60;
+
+let remainingTime = workTime;
+let isWorking = true;
+let timer;
+
+const startTimer = () => {
+  timer = setInterval(() => {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`Remaining time: ${Math.floor(remainingTime / 60)}:${remainingTime % 60}`);
+    remainingTime--;
+
+    if (remainingTime < 0) {
+      if (isWorking) {
+        notifier.notify({
+          title: 'Pomodoro Timer',
+          message: 'Time to take a break!'
+        });
+        remainingTime = breakTime;
+      } else {
+        notifier.notify({
+          title: 'Pomodoro Timer',
+          message: 'Time to work!'
+        });
+        remainingTime = workTime;
+      }
+      isWorking = !isWorking;
+    }
+  }, 1000);
 }
 
-startPomodoro();
+rl.on('line', () => {
+  if (timer) {
+    clearInterval(timer);
+  }
+  startTimer();
+});
+
+rl.on('close', () => {
+  clearInterval(timer);
+});
